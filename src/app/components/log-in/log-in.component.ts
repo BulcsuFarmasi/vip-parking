a411959166a5d89118fc2978b641dfefdb408154
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-03-10 20:29:39 +0100

    add log in component fields, dependecies, methods

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Guard } from 'src/app/models/guard';
+import { GuardService } from 'src/app/services/guard.service';
 
 @Component({
   selector: 'app-log-in',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  guards:Guard[]
+  
+  constructor(private guardService:GuardService, private router:Router) { }
 
   ngOnInit() {
+    this.guards = this.guardService.getGuards(); 
+  }
+
+  logIn (id:number) {
+      this.guardService.setCurrentGuard(id);
+      this.router.navigate(['/dashboard'])
   }
 
 }
