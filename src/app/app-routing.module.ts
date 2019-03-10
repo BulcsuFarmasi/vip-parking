7a4c5117ee7dc0aae193ab25457282b784df3060
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-03-10 19:58:13 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'log-in', component: LogInComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/log-in' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
