98553d1f4f49d3b47eb08d53962dabf89591879f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-03-11 09:13:59 +0100

    add properties

--- src/app/components/new-parking/new-parking.component.ts
+++ src/app/components/new-parking/new-parking.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewParkingComponent implements OnInit {
 
+  formVisible:boolean;
+  isBelowCapacity:boolean;
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
